be3f63e3e3b4bfeb43802116d9081b5422a4f939
Ignacio <ignaciodlta@gmail.com>
2026-01-08 18:47:26 -0600

    Add Windows resource compilation for Tauri

--- build.rs
+++ build.rs
@@ -1,5 +1,10 @@
 use std::io::Result;
 fn main() -> Result<()> {
     prost_build::compile_protos(&["protos/module.proto"], &["protos/"])?;
+    #[cfg(target_os = "windows")]
+    {
+        let mut res = tauri_winres::WindowsResourse::new();
+        res.compile()?;
+    }
     Ok(())
 }
